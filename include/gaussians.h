1702f7791025dfb8204e8cfaca18d384fd66bb03
Fernando mata <matarabadanf@gmail.com>
2025-09-09 21:30:19 +0200

    Obara-Saika Kinetic energy integrals and tests

--- include/gaussians.h
+++ include/gaussians.h
@@ -5,6 +5,10 @@ double cartesian_gaussian(unsigned int ii, double A_x, double a, double x);
 double gaussian_overlap_distribution(unsigned int ii, double A_x, double a, unsigned int jj, double B_x, double b, double x);
 double Overlap_OS(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj);
 double *Overlap_OS_matrix(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj);
+double Kinetic_OS(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj);
+double *kinetic_OS_matrix(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj);
+
+
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
--- src/integral_evaluation/gaussians.c
+++ src/integral_evaluation/gaussians.c
@@ -24,87 +24,61 @@ double gaussian_overlap_distribution(unsigned int ii, double A_x, double a, unsi
     return exp(-mu * pow(X_ab, 2)) * pow(x_a, ii) *  pow(x_b, jj) * exp(-p * pow(x_p, 2));
 }
 
-#define MATRIX_VAL(i, j, n_cols, matrix) matrix[ (i) * n_cols + (j) ]
+double *Overlap_OS_matrix(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj) {
 
-double *Overlap_OS_matrix(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj){
+    // builds the Obara-Saika recurrence matrix for overlap integrals. 
+
+    // Each element (using i-recurrence) is given by:
+    //    S_{i+1,\,j} = X_{PA} S_{ij} + \frac{1}{2p}(iS_{i-1,\,j} + jS_{i,\,j-1})
+    // or:
+    //    S_{i,\,j} = X_{PA} S_{i-1,j} + \frac{1}{2p}((i-1)*S_{i-2,\,j} + jS_{i-1,\,j-1})
 
     unsigned int max_dim = MAX(ii, jj) + 1;
-
-    // printf("\nMax_dim is %d, I = %d, j=%d", max_dim, ii, jj );
-
-    double *angular_momentum_matrix = malloc(max_dim * max_dim * sizeof(double));
+    double *S = malloc(max_dim * max_dim * sizeof(double));
+    if (!S) return NULL;  // safe malloc check
 
     double p = a + b;
-    double mu = (a * b)/p;
-    double X_ab = (Bx-Ax);
-    double X_pa = b/p * X_ab;
-    double X_pb = -a/p * X_ab;
+    double mu = a * b / p;
+    double X_ab = Bx - Ax;
+    double X_pa = b / p * X_ab;
+    double X_pb = -a / p * X_ab;
 
+    // Base case
+    S[0] = sqrt(PI / p) * exp(-mu * X_ab * X_ab);
 
-    // ground case
-    MATRIX_VAL(0, 0, max_dim, angular_momentum_matrix) = pow(PI/p, 0.5) * exp(-mu * pow(X_ab, 2));
-
-    // i row
-    for (int i = 1; i < max_dim; i++){
-
-        if (i-2 < 0){
-            MATRIX_VAL(i, 0, max_dim, angular_momentum_matrix) = X_pa * MATRIX_VAL(i-1, 0, max_dim, angular_momentum_matrix);
-
-            // printf("\n\n\n%f",  MATRIX_VAL(i, 0, max_dim, angular_momentum_matrix));
-        }
-
-        else{
-            MATRIX_VAL(i, 0, max_dim, angular_momentum_matrix) = X_pa * MATRIX_VAL(i-1, 0, max_dim, angular_momentum_matrix) +
-             1/(2*p) *((i-1) * MATRIX_VAL(i-2, 0, max_dim, angular_momentum_matrix));
-            
-            // printf("\n\n\n\n%f",  MATRIX_VAL(i, 0, max_dim, angular_momentum_matrix));
-        }
+    // First column (j = 0)
+    for (unsigned int i = 1; i < max_dim; i++) {
+        double term2 = (i >= 2) ? (i - 1) / (2.0 * p) * S[(i - 2) * max_dim] : 0.0; // if i >= 2, then (i - 1) / (2.0 * p) * S[(i - 2) * max_dim] : else This is the ternary operator in c 
+        S[i * max_dim] = X_pa * S[(i - 1) * max_dim] + term2;
     }
 
-    // j_row
-    for (int j = 1; j < max_dim; j++){
-
-        if (j-2 < 0){
-            MATRIX_VAL(0, j, max_dim, angular_momentum_matrix) = X_pb * MATRIX_VAL(0, j-1, max_dim, angular_momentum_matrix);
-
-            // printf("\n\n\n%f",  MATRIX_VAL(0, j, max_dim, angular_momentum_matrix));
-        }
-
-        else{
-            MATRIX_VAL(0, j, max_dim, angular_momentum_matrix) = X_pb * MATRIX_VAL(0, j-1, max_dim, angular_momentum_matrix) +
-             1/(2*p) *((j-1) * MATRIX_VAL(0, j-2, max_dim, angular_momentum_matrix));
-            
-            // printf("\n\n\n\n%f",  MATRIX_VAL(0, j, max_dim, angular_momentum_matrix));
-        }
+    // First row (i = 0)
+    for (unsigned int j = 1; j < max_dim; j++) {
+        double term2 = (j >= 2) ? (j - 1) / (2.0 * p) * S[j - 2] : 0.0;
+        S[j] = X_pb * S[j - 1] + term2;
     }
 
-
-    // general case 
-    for (int total = 1; total < max_dim; total++) {
-        for (int i = total; i < max_dim; i++) {
-            double term1 = X_pa * MATRIX_VAL(i-1, total, max_dim, angular_momentum_matrix);
+    // General case (i > 0, j > 0) using i-recurrence 
+    for (unsigned int i = 1; i < max_dim; i++) {
+        for (unsigned int j = 1; j < max_dim; j++) {
+            double term1 = X_pa * S[(i - 1) * max_dim + j];
             double term2 = 0.0;
-            if (i-2 >= 0)
-                term2 += (i-1) * MATRIX_VAL(i-2, total, max_dim, angular_momentum_matrix);
-            if (total-1 >= 0)
-                term2 += (total) * MATRIX_VAL(i-1, total-1, max_dim, angular_momentum_matrix);
-            term2 *= 1.0/(2*p);
-            MATRIX_VAL(i, total, max_dim, angular_momentum_matrix) = term1 + term2;
-        }
-        for (int j = total; j < max_dim; j++) {
-            double term1 = X_pb * MATRIX_VAL(total, j-1, max_dim, angular_momentum_matrix);
-            double term2 = 0.0;
-            if (total-1 >= 0 && j-1 >= 0)
-                term2 += (total) * MATRIX_VAL(total-1, j-1, max_dim, angular_momentum_matrix);
-            if (j-2 >= 0)
-                term2 += (j-1) * MATRIX_VAL(total, j-2, max_dim, angular_momentum_matrix);
-            term2 *= 1.0/(2*p);
-            MATRIX_VAL(total, j, max_dim, angular_momentum_matrix) = term1 + term2;
+            double term3 = 0.0;
+
+            // The second and third terms are evaluated separately way, and it is necessary to check for negative indices: 
+            if (i >= 2){
+                term2 += (i - 1) / (2.0 * p) * S[(i - 2) * max_dim + j];
+            }
+
+            term3 = j / (2.0 * p) * S[(i - 1) * max_dim + (j - 1)];
+
+            S[i * max_dim + j] = term1 + term2 + term3;
         }
     }
 
-    return angular_momentum_matrix;
+    return S;
 }
+
 
 double Overlap_OS(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj){
 
@@ -114,6 +88,77 @@ double Overlap_OS(double Ax, double Bx, double a, double b, unsigned int ii, uns
     double result = MATRIX_VAL(ii, jj, max_dim, ang_matrix);
 
     free(ang_matrix);
+
+    return result;
+}
+
+double *kinetic_OS_matrix(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj){
+
+
+    unsigned int max_dim = MAX(ii, jj) + 1;
+    double *S =  Overlap_OS_matrix(Ax, Bx, a, b, ii, jj);
+    double *T = malloc(max_dim * max_dim * sizeof(double));
+    if (!S) return NULL;  // safe malloc check
+    if (!T) return NULL; 
+
+
+    double p = a + b;
+    double mu = a * b / p;
+    double X_ab = Bx - Ax;
+    double X_pa = b / p * X_ab;
+    double X_pb = -a / p * X_ab;
+    
+
+    // Base case
+    T[0] = (a - 2 * pow(a, 2) * (pow(X_pa, 2) + 1/(2*p))) * S[0];
+
+    //first column
+    for (unsigned int i = 1; i < max_dim; i++) {
+        double term1 = X_pa * MATRIX_VAL(i-1, 0, max_dim, T); 
+        double term2 = (i >= 2) ? 1/(2*p) * (i-1) * MATRIX_VAL(i-2, 0, max_dim, T): 0; 
+        double term3 = b/p * 2 * a * MATRIX_VAL(i, 0, max_dim, S); 
+        double term4 = (i >= 2) ?  - b/p * (i-1) * MATRIX_VAL(i-2, 0, max_dim, S) : 0; 
+
+        MATRIX_VAL(i, 0, max_dim, T) = term1 + term2 + term3 + term4;
+    }
+
+    
+    //first row
+    for (unsigned int j = 1; j < max_dim; j++) {
+        double term1 = X_pb * MATRIX_VAL(0, j-1, max_dim, T); 
+        double term2 = (j >= 2) ? 1/(2*p) * (j-1) * MATRIX_VAL(0, j-2, max_dim, T): 0; 
+        double term3 = a/p * 2 * b * MATRIX_VAL(0, j, max_dim, S); 
+        double term4 = (j >= 2) ?  - a/p * (j-1) * MATRIX_VAL(0, j-2, max_dim, S) : 0; 
+
+        MATRIX_VAL(0, j, max_dim, T) = term1 + term2 + term3 + term4;
+    }
+
+
+        // General case (i > 0, j > 0) using i-recurrence 
+    for (unsigned int i = 1; i < max_dim; i++) {
+        for (unsigned int j = 1; j < max_dim; j++) {
+            double term1 = X_pa * MATRIX_VAL(i-1, j, max_dim, T); 
+            double term2 = (i >= 2) ? 1/(2*p) * (i-1) * MATRIX_VAL(i-2, j, max_dim, T): 0; 
+            double term2_2 = 1/(2*p) * (j) * MATRIX_VAL(i-1, j-1, max_dim, T); 
+            double term3 = b/p * 2 * a * MATRIX_VAL(i, j, max_dim, S); 
+            double term4 = (i >= 2) ?  - b/p * (i-1) * MATRIX_VAL(i-2, j, max_dim, S) : 0; 
+
+
+            T[i * max_dim + j] = term1 + term2 + term2_2 + term3 + term4;
+        }
+    }
+
+    return T;
+}
+
+double Kinetic_OS(double Ax, double Bx, double a, double b, unsigned int ii, unsigned int jj){
+
+    unsigned int max_dim = MAX(ii, jj) + 1;
+    double *kinetic_matrix = kinetic_OS_matrix(Ax, Bx, a, b, ii, jj);
+
+    double result = MATRIX_VAL(ii, jj, max_dim, kinetic_matrix);
+
+    free(kinetic_matrix);
 
     return result;
 }
