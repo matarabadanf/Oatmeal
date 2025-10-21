98e63ea123fc16eedbeb8bacee1967a1453a6cfc
matarabadanf <matarabadanf@gmail.com>
2025-10-21 15:06:44 +0200

    Set up tests and made installable

--- py_mods/src/RHF.py
+++ py_mods/src/RHF.py
@@ -330,8 +330,6 @@ def RHF(
     E_RHF = E_iter
     return converged, E_RHF, e_values, C_munu, P
 
-
-
 if __name__ == "__main__":
     """
     S_sto3g_H2 = np.loadtxt('./data/s_H2.dat')
@@ -352,7 +350,7 @@ if __name__ == "__main__":
     print(e_values)
     """
 
-
+    # Li 6-31g test
     S_sto3g_li    = np.loadtxt('../tests/data/Li_plus_S_6-31g.dat')
     T_sto3g_li    = np.loadtxt('../tests/data/Li_plus_kin_6-31g.dat')
     V_sto3g_li    = np.loadtxt('../tests/data/Li_plus_vnuc_6-31g.dat')
@@ -370,6 +368,25 @@ if __name__ == "__main__":
     converged, E_hf, E_e_values, C_munu, P = RHF(S_sto3g_li, T_sto3g_li, V_sto3g_li, eri_sto3g_li, n_electrons=2, max_iter=100, threshold=1E-14, p_guess='core', verbose=True)
     assert converged, "Calculation did not converge"
     assert abs(E_hf - E_hf_sto3g_li) < 1E-8, f"SCF energy does not match reference value {E_hf} != {E_hf_sto3g_li}"
+
+    # Be ccpvdz test 
+    S_ccpvdz_Be    = np.loadtxt('../tests/data/Be_S_ccpvdz.dat')
+    T_ccpvdz_Be    = np.loadtxt('../tests/data/Be_kin_ccpvdz.dat')
+    V_ccpvdz_Be    = np.loadtxt('../tests/data/Be_vnuc_ccpvdz.dat')
+    eri_ccpvdz_Be  = np.load('../tests/data/Be_eri_ccpvdz.npy')
+    E_hf_ccpvdz_Be = np.load('../tests/data/Be_e_hf_ccpvdz.npy')
+
+    idn = np.identity(len(S_ccpvdz_Be))
+
+    # test 1: successful transformation matrix
+    X = transformation_matrix(S_ccpvdz_Be)
+    transformed = X.T @ S_ccpvdz_Be @ X      # sould be the identity    
+    assert equiv_matrix(transformed, idn), "transformation matrix calculation failed"
+
+    # test 2: SCF convergence for Be in ccpvdz
+    converged, E_hf, E_e_values, C_munu, P = RHF(S_ccpvdz_Be, T_ccpvdz_Be, V_ccpvdz_Be, eri_ccpvdz_Be, n_electrons=4, max_iter=100, threshold=1E-14, p_guess='core', verbose=True)
+    assert converged, "Calculation did not converge"
+    assert abs(E_hf - E_hf_ccpvdz_Be) < 1E-8, f"SCF energy does not match reference value {E_hf} != {E_hf_ccpvdz_Be}"
 
 
     
