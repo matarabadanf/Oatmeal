c1c4294c03dea213729d547966e3d402bf9527fb
matarabadanf <matarabadanf@gmail.com>
2026-01-21 15:06:45 +0100

    Created the CGTO module. Added tests for l=0 and l=1.

---
+++ py_mods/src/integrals/CGTO.py
@@ -0,0 +1,496 @@
+from py_mods.src.integrals.GTO import (
+    GTO,
+    create_normalized_GTO,
+    generate_angular_momentum_projections,
+    S_3D,
+    T_3D,
+    V_3D,
+    eri,
+)
+from dataclasses import dataclass
+from numpy.typing import NDArray
+import numpy as np
+
+
+###############################################################################
+#                         CGTO Class and Constructor                          #
+###############################################################################
+@dataclass
+class CGTOClass:
+    """
+    Contracted Gaussian Type Orbital (CGTO) class.
+
+    Attributes
+    ----------
+    R : NDArray[np.float64]
+        Position of the basis.
+    exps : NDArray[np.float64]
+        Exponents of the primitives.
+    N_a : float
+        Normalization constant.
+    d_i : NDArray[np.float64]
+        Contraction coefficients.
+    total_l : int
+        Total angular momentum.
+    l_dim : int
+        Number of projections with this angular momentum.
+    l_projections : NDArray[np.int32]
+        All cartesian projections l.
+    primitives : list[GTO]
+        List of primitive GTOs.
+    """
+
+    R: NDArray[np.float64]  # position of basis
+    exps: NDArray[np.float64]  # exponents
+    N_a: float  # normalization constant
+    d_i: NDArray[np.float64]  # contraction coefficients
+    total_l: int
+    l_dim: int  # number of projections with this angular momentum
+    l_projections: NDArray[np.int32]  # all projections for this angular momentum
+    primitives: list[GTO]  # actually needed dut to normalization constants
+
+
+def create_CGTOClass(
+    R: NDArray[np.float64],
+    exps: NDArray[np.float64],
+    d_i: NDArray[np.float64],
+    total_L: int,
+) -> CGTOClass:
+    """
+    Factory function to create a GTOClass
+
+    Parameters
+    ----------
+    R : NDArray[np.float64]
+        Center of the GTO.
+    exps : NDArray[np.float64]
+        Exponents of the primitives.
+    d_i : NDArray[np.float64]
+        Contraction coefficients of the primitives.
+    total_L : int
+        Total angular momentum of the GTO.
+
+    Returns
+    -------
+    GTOClass
+        Normalized GTOClass object.
+    """
+    l_projections = generate_angular_momentum_projections(total_L)
+    l_dim = l_projections.shape[0]
+    primitives = [create_normalized_GTO(R, exp, total_L) for exp in exps]
+
+    unnormalized_cgto = CGTOClass(
+        R=R,
+        exps=exps,
+        d_i=d_i,
+        N_a=1,
+        total_l=total_L,
+        l_dim=l_dim,
+        l_projections=l_projections,
+        primitives=primitives,
+    )
+
+    N_a = calculate_normalization_constant(unnormalized_cgto)
+
+    unnormalized_cgto.N_a = N_a
+
+    return unnormalized_cgto
+
+
+def calculate_normalization_constant(cont: CGTOClass) -> float:
+    """
+    Calculate normalization constant for a contracted GTO.
+
+    Parameters
+    ----------
+    cont : CGTOClass
+        Contracted GTO.
+
+    Returns
+    -------
+    float
+        Normalization constant.
+    """
+    S = S_GTO_proj(cont, 0, cont, 0)
+    N = 1.0 / np.sqrt(S)
+
+    return N
+
+
+###############################################################################
+#                             Overlap integrals                               #
+###############################################################################
+
+
+def S_GTO_mat(cont_1: CGTOClass, cont_2: CGTOClass):
+    """
+    Calculate overlap matrix between two contracted GTOs.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+
+    Returns
+    -------
+    NDArray[np.float64] of shape (l_dim_1, l_dim_2)
+        Overlap integral matrix.
+    """
+    S_mat = np.zeros((cont_1.l_dim, cont_2.l_dim))
+
+    for p1 in range(cont_1.l_dim):
+        for p2 in range(cont_1.l_dim):
+            S_mat[p1, p2] = S_GTO_proj(cont_1, p1, cont_2, p2)
+
+    return S_mat * cont_1.N_a * cont_2.N_a
+
+
+def S_GTO_proj(cont_1: CGTOClass, proj_idx_1: int, cont_2: CGTOClass, proj_idx_2: int):
+    """
+    Calculate overlap integral between two contracted GTOs
+    given their l projections.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    proj_idx_1 : int
+        Projection index for the first contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    proj_idx_2 : int
+        Projection index for the second contracted GTO.
+
+    Returns
+    -------
+    float
+        Overlap integral value.
+    """
+    projection_vec_1 = cont_1.l_projections[proj_idx_1]
+    projection_vec_2 = cont_2.l_projections[proj_idx_2]
+
+    S: float = 0.0
+    for i, gto_1 in enumerate(cont_1.primitives):
+        for j, gto_2 in enumerate(cont_2.primitives):
+            N_a = gto_1.normalization_constants[proj_idx_1]
+            N_b = gto_2.normalization_constants[proj_idx_2]
+            primitive_overlap = (
+                cont_1.d_i[i]
+                * cont_2.d_i[j]
+                * S_3D(
+                    gto_1,
+                    projection_vec_1,
+                    N_a,
+                    gto_2,
+                    projection_vec_2,
+                    N_b,
+                )
+            )
+
+            S += primitive_overlap
+
+    return S
+
+
+###############################################################################
+#                         Kinetic energy integrals                            #
+###############################################################################
+
+
+def T_GTO_mat(cont_1: CGTOClass, cont_2: CGTOClass):
+    """
+    Calculate kinetic energy integral matrix between two contracted GTOs.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+
+    Returns
+    -------
+    NDArray[np.float64] of shape (l_dim_1, l_dim_2)
+        Kinetic energy integral matrix.
+    """
+    T_mat = np.zeros((cont_1.l_dim, cont_2.l_dim))
+
+    for p1 in range(cont_1.l_dim):
+        for p2 in range(cont_1.l_dim):
+            T_mat[p1, p2] = T_GTO_proj(cont_1, p1, cont_2, p2)
+
+    return T_mat * cont_1.N_a * cont_2.N_a
+
+
+def T_GTO_proj(cont_1: CGTOClass, proj_idx_1: int, cont_2: CGTOClass, proj_idx_2: int):
+    """
+    Calculate kinetic energy integral between two contracted GTOs
+    given their l projections.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    proj_idx_1 : int
+        Projection index for the first contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    proj_idx_2 : int
+        Projection index for the second contracted GTO.
+
+    Returns
+    -------
+    float
+        kinetic energy integral value.
+    """
+    projection_vec_1 = cont_1.l_projections[proj_idx_1]
+    projection_vec_2 = cont_2.l_projections[proj_idx_2]
+
+    T: float = 0.0
+    for i, gto_1 in enumerate(cont_1.primitives):
+        for j, gto_2 in enumerate(cont_2.primitives):
+            N_a = gto_1.normalization_constants[proj_idx_1]
+            N_b = gto_2.normalization_constants[proj_idx_2]
+            T_primitive = (
+                cont_1.d_i[i]
+                * cont_2.d_i[j]
+                * T_3D(
+                    gto_1,
+                    projection_vec_1,
+                    N_a,
+                    gto_2,
+                    projection_vec_2,
+                    N_b,
+                )
+            )
+
+            T += T_primitive
+
+    return T
+
+
+###############################################################################
+#                        Potential energy integrals                           #
+###############################################################################
+
+
+def V_GTO_mat(
+    cont_1: CGTOClass,
+    cont_2: CGTOClass,
+    atom_pos: NDArray[np.float64],
+    atom_charges: NDArray[np.float64],
+):
+    """
+    Calculate the potential energy integral matrix between two contracted GTOs.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    atom_pos : NDArray[np.float64] of shape (n_atoms, 3)
+        Positions of the atoms.
+    atom_charges : NDArray[np.float64] of shape (n_atoms,)
+        Charges of the atoms.
+
+    Returns
+    -------
+    NDArray[np.float64] of shape (l_dim_1, l_dim_2)
+        Potential energy integral matrix.
+    """
+    S_mat = np.zeros((cont_1.l_dim, cont_2.l_dim))
+
+    for p1 in range(cont_1.l_dim):
+        for p2 in range(cont_1.l_dim):
+            S_mat[p1, p2] = V_GTO_proj(cont_1, p1, cont_2, p2, atom_pos, atom_charges)
+
+    return S_mat * cont_1.N_a * cont_2.N_a
+
+
+def V_GTO_proj(
+    cont_1: CGTOClass,
+    proj_idx_1: int,
+    cont_2: CGTOClass,
+    proj_idx_2: int,
+    atom_pos: NDArray[np.float64],
+    atom_charges: NDArray[np.float64],
+):
+    """
+    Calculate potential energy integral between two contracted GTOs
+    given their l projections.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    proj_idx_1 : int
+        Projection index for the first contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    proj_idx_2 : int
+        Projection index for the second contracted GTO.
+    atom_pos : NDArray[np.float64] of shape (n_atoms, 3)
+        Positions of the atoms.
+    atom_charges : NDArray[np.float64] of shape (n_atoms,)
+        Charges of the atoms.
+
+    Returns
+    -------
+    float
+        Potential energy integral value.
+    """
+    if len(atom_pos) != len(atom_charges):
+        raise ValueError("atom_pos and atom_charges must have the same length.")
+
+    projection_vec_1 = cont_1.l_projections[proj_idx_1]
+    projection_vec_2 = cont_2.l_projections[proj_idx_2]
+
+    V: float = 0.0
+    for i, gto_1 in enumerate(cont_1.primitives):
+        for j, gto_2 in enumerate(cont_2.primitives):
+            for a in range(len(atom_charges)):
+                pos = atom_pos[a]
+                charge = atom_charges[a]
+                N_a = gto_1.normalization_constants[proj_idx_1]
+                N_b = gto_2.normalization_constants[proj_idx_2]
+                V_primitive = (
+                    cont_1.d_i[i]
+                    * cont_2.d_i[j]
+                    * V_3D(
+                        gto_1,
+                        projection_vec_1,
+                        N_a,
+                        gto_2,
+                        projection_vec_2,
+                        N_b,
+                        charge,
+                        pos,
+                    )
+                )
+
+                V += V_primitive
+
+    return V
+
+
+###############################################################################
+#                    Electron repulsion energy integrals                      #
+###############################################################################
+
+
+def Eri_GTO_tensor(
+    cont_1: CGTOClass, cont_2: CGTOClass, cont_3: CGTOClass, cont_4: CGTOClass
+) -> NDArray[np.float64]:
+    """
+    Calculate ERI tensor between four contracted GTOs.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    cont_3 : CGTOClass
+        Third contracted GTO.
+    cont_4 : CGTOClass
+        Fourth contracted GTO.
+
+    Returns
+    -------
+    NDArray[np.float64] of shape (l_dim_1, l_dim_2, l_dim_3, l_dim_4)
+        ERI tensor.
+    """
+    eri_mat = np.zeros((cont_1.l_dim, cont_2.l_dim, cont_3.l_dim, cont_4.l_dim))
+
+    for p1 in range(cont_1.l_dim):
+        for p2 in range(cont_1.l_dim):
+            for p3 in range(cont_3.l_dim):
+                for p4 in range(cont_4.l_dim):
+                    eri_mat[p1, p2, p3, p4] = eri_GTO_proj(
+                        cont_1, p1, cont_2, p2, cont_3, p3, cont_4, p4
+                    )
+
+    eri_tensor = eri_mat * cont_1.N_a * cont_2.N_a * cont_3.N_a * cont_4.N_a
+
+    return eri_tensor
+
+
+def eri_GTO_proj(
+    cont_1: CGTOClass,
+    proj_idx_1: int,
+    cont_2: CGTOClass,
+    proj_idx_2: int,
+    cont_3: CGTOClass,
+    proj_idx_3: int,
+    cont_4: CGTOClass,
+    proj_idx_4: int,
+) -> float:
+    """
+    Calculate ERI between four contracted GTOs for given their l projections.
+
+    Parameters
+    ----------
+    cont_1 : CGTOClass
+        First contracted GTO.
+    proj_idx_1 : int
+        Projection index for the first contracted GTO.
+    cont_2 : CGTOClass
+        Second contracted GTO.
+    proj_idx_2 : int
+        Projection index for the second contracted GTO.
+    cont_3 : CGTOClass
+        Third contracted GTO.
+    proj_idx_3 : int
+        Projection index for the third contracted GTO.
+    cont_4 : CGTOClass
+        Fourth contracted GTO.
+    proj_idx_4 : int
+        Projection index for the fourth contracted GTO.
+
+    Returns
+    -------
+    float
+        ERI value.
+    """
+    projection_vec_1 = cont_1.l_projections[proj_idx_1]
+    projection_vec_2 = cont_2.l_projections[proj_idx_2]
+    projection_vec_3 = cont_3.l_projections[proj_idx_3]
+    projection_vec_4 = cont_4.l_projections[proj_idx_4]
+
+    eri_val: float = 0.0
+    for i, gto_1 in enumerate(cont_1.primitives):
+        for j, gto_2 in enumerate(cont_2.primitives):
+            for k, gto_3 in enumerate(cont_3.primitives):
+                for l, gto_4 in enumerate(cont_4.primitives):
+                    N_a = gto_1.normalization_constants[proj_idx_1]
+                    N_b = gto_2.normalization_constants[proj_idx_2]
+                    N_c = gto_3.normalization_constants[proj_idx_3]
+                    N_d = gto_4.normalization_constants[proj_idx_4]
+
+                    abcd_eri = (
+                        cont_1.d_i[i]
+                        * cont_2.d_i[j]
+                        * cont_3.d_i[k]
+                        * cont_4.d_i[l]
+                        * eri(
+                            gto_1,
+                            projection_vec_1,
+                            N_a,
+                            gto_2,
+                            projection_vec_2,
+                            N_b,
+                            gto_3,
+                            projection_vec_3,
+                            N_c,
+                            gto_4,
+                            projection_vec_4,
+                            N_d,
+                        )
+                    )
+
+                    eri_val += abcd_eri
+
+    return eri_val
---
+++ tests/py_tests/CGTO_test.py
@@ -0,0 +1,158 @@
+from py_mods.src.integrals.CGTO import (
+    create_CGTOClass,
+    S_GTO_mat,
+    generate_angular_momentum_projections,
+    T_GTO_mat,
+    V_GTO_mat,
+    Eri_GTO_tensor,
+)
+
+from pyscf import gto
+import numpy as np
+
+
+def test_1s1s():
+    # STO3g example:
+    l = 0
+    l_tags = ["S", "P", "D", "F", "G", "H"]
+    l_projs = len(generate_angular_momentum_projections(l))
+
+    r1 = np.array([0.0, 0.0, 0.0])
+    r2 = np.array([0.0, 0.0, 1.4])
+    H_exps = np.array([3.42525091, 0.62391373, 0.16885540])
+    H_coeffs = np.array([0.15432897, 0.53532814, 0.44463454])
+
+    atom_pos = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.4]])
+    atom_charges = np.array([1.0, 1.0])
+
+    # Construct the CGTO
+    H_1s_1 = create_CGTOClass(r1, H_exps, H_coeffs, l)
+    H_1s_2 = create_CGTOClass(r2, H_exps, H_coeffs, l)
+
+    S_1s1s_sto3g_self = S_GTO_mat(H_1s_1, H_1s_2)
+    T_1s1s_sto3g_self = T_GTO_mat(H_1s_1, H_1s_2)
+    V_1s1s_sto3g_self = V_GTO_mat(H_1s_1, H_1s_2, atom_pos, atom_charges)
+    Eri_1s1s1s1s_self = Eri_GTO_tensor(H_1s_1, H_1s_2, H_1s_1, H_1s_1)
+
+    mol = gto.M(
+        atom="H 0 0 0; H 0 0 1.4",
+        unit="Bohr",
+        basis={
+            "H": gto.basis.parse(
+                f"""
+                    H {l_tags[l]}
+                    3.42525091    0.15432897
+                    0.62391373    0.53532814
+                    0.16885540    0.44463454
+                """
+            )
+        },
+        cart=True,
+    )
+
+    mol.build()
+
+    overlap = mol.intor("int1e_ovlp")
+    kin = mol.intor("int1e_kin")
+    V = mol.intor("int1e_nuc")
+    ref_eri = mol.intor("int2e")
+
+    # pyscf does not normalize in cartesian like this, we have to renormalize to compare
+    norm_vec = 1.0 / np.sqrt(np.diag(overlap))
+
+    overlap *= norm_vec[:, None]
+    overlap *= norm_vec[None, :]
+
+    kin *= norm_vec[:, None]
+    kin *= norm_vec[None, :]
+
+    V *= norm_vec[:, None]
+    V *= norm_vec[None, :]
+
+    ref_eri *= norm_vec[:, None, None, None]
+    ref_eri *= norm_vec[None, :, None, None]
+    ref_eri *= norm_vec[None, None, :, None]
+    ref_eri *= norm_vec[None, None, None, :]
+
+    assert np.allclose(S_1s1s_sto3g_self, overlap[l_projs:, :l_projs])
+    assert np.allclose(T_1s1s_sto3g_self, kin[l_projs:, :l_projs])
+    assert np.allclose(V_1s1s_sto3g_self, V[l_projs:, :l_projs])
+    assert np.allclose(
+        Eri_1s1s1s1s_self, ref_eri[l_projs:, :l_projs, l_projs:, l_projs:]
+    )
+
+
+def test_2p2p():
+    # STO3g example:
+    l = 1
+    l_tags = ["S", "P", "D", "F", "G", "H"]
+    l_projs = len(generate_angular_momentum_projections(l))
+
+    r1 = np.array([0.0, 0.0, 0.0])
+    r2 = np.array([0.0, 0.0, 1.4])
+    H_exps = np.array([3.42525091, 0.62391373, 0.16885540])
+    H_coeffs = np.array([0.15432897, 0.53532814, 0.44463454])
+
+    atom_pos = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.4]])
+    atom_charges = np.array([1.0, 1.0])
+
+    # Construct the CGTO
+    H_1s_1 = create_CGTOClass(r1, H_exps, H_coeffs, l)
+    H_1s_2 = create_CGTOClass(r2, H_exps, H_coeffs, l)
+
+    S_1s1s_sto3g_self = S_GTO_mat(H_1s_1, H_1s_2)
+    T_1s1s_sto3g_self = T_GTO_mat(H_1s_1, H_1s_2)
+    V_1s1s_sto3g_self = V_GTO_mat(H_1s_1, H_1s_2, atom_pos, atom_charges)
+    Eri_1s1s1s1s_self = Eri_GTO_tensor(H_1s_1, H_1s_2, H_1s_1, H_1s_1)
+
+    mol = gto.M(
+        atom="H 0 0 0; H 0 0 1.4",
+        unit="Bohr",
+        basis={
+            "H": gto.basis.parse(
+                f"""
+                    H {l_tags[l]}
+                    3.42525091    0.15432897
+                    0.62391373    0.53532814
+                    0.16885540    0.44463454
+                """
+            )
+        },
+        cart=True,
+    )
+
+    mol.build()
+
+    overlap = mol.intor("int1e_ovlp")
+    kin = mol.intor("int1e_kin")
+    V = mol.intor("int1e_nuc")
+    ref_eri = mol.intor("int2e")
+
+    # pyscf does not normalize in cartesian like this, we have to renormalize to compare
+    norm_vec = 1.0 / np.sqrt(np.diag(overlap))
+
+    overlap *= norm_vec[:, None]
+    overlap *= norm_vec[None, :]
+
+    kin *= norm_vec[:, None]
+    kin *= norm_vec[None, :]
+
+    V *= norm_vec[:, None]
+    V *= norm_vec[None, :]
+
+    ref_eri *= norm_vec[:, None, None, None]
+    ref_eri *= norm_vec[None, :, None, None]
+    ref_eri *= norm_vec[None, None, :, None]
+    ref_eri *= norm_vec[None, None, None, :]
+
+    assert np.allclose(S_1s1s_sto3g_self, overlap[l_projs:, :l_projs])
+    assert np.allclose(T_1s1s_sto3g_self, kin[l_projs:, :l_projs])
+    assert np.allclose(V_1s1s_sto3g_self, V[l_projs:, :l_projs])
+    assert np.allclose(
+        Eri_1s1s1s1s_self, ref_eri[l_projs:, :l_projs, l_projs:, l_projs:]
+    )
+
+
+if __name__ == "__main__":
+    test_1s1s()
+    test_2p2p()
