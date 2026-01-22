d1badbc5bbb0b60d62a84d6f71afe3f1b610e976
matarabadanf <matarabadanf@gmail.com>
2026-01-22 17:10:05 +0100

    Solved conflict.

--- py_mods/src/integrals/Basis.py
+++ py_mods/src/integrals/Basis.py
@@ -12,11 +12,7 @@ import numpy as np
 
 
 @dataclass
-<<<<<<< HEAD
-class BasisSet:
-=======
 class BasisSetClass:
->>>>>>> 4f58fba (BSE api.)
     CGTOs: List[CGTOClass]
     n_CGTOs: int
     matrix_indices: NDArray[np.int16]
@@ -25,9 +21,6 @@ class BasisSetClass:
     q_atoms: NDArray[np.float64]
 
 
-<<<<<<< HEAD
-def construct_basis_from_lists(cgto_list, r_atom_list, q_atom_list) -> BasisSet:
-=======
 def construct_basis_from_lists(
     cgto_list: list[CGTOClass],
     r_atom_list: NDArray[np.float64],
@@ -51,7 +44,6 @@ def construct_basis_from_lists(
         An instance of BasisSetClass containing the provided CGTOs and atomic data.
     """
 
->>>>>>> 4f58fba (BSE api.)
     if len(r_atom_list) != len(q_atom_list):
         raise ValueError("Length of r_atom_list and q_atom_list must be the same.")
 
@@ -59,11 +51,7 @@ def construct_basis_from_lists(
     cgto_indices = [sum(projections[:i]) for i in range(len(cgto_list))]
     cgto_indices.append(projections[-1] + cgto_indices[-1])
 
-<<<<<<< HEAD
-    basis_set = BasisSet(
-=======
     basis_set = BasisSetClass(
->>>>>>> 4f58fba (BSE api.)
         CGTOs=cgto_list,
         n_CGTOs=len(cgto_list),
         matrix_indices=cgto_indices,
@@ -75,9 +63,6 @@ def construct_basis_from_lists(
     return basis_set
 
 
-<<<<<<< HEAD
-def S_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-=======
 def S_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     """
     Calculate overlap matrix of a given basis set.
@@ -92,7 +77,6 @@ def S_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     NDArray[np.float64]
         Overlap matrix.
     """
->>>>>>> 4f58fba (BSE api.)
     n_CGTOs = basis_set.n_CGTOs
     S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
 
@@ -111,9 +95,6 @@ def S_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     return S_mat
 
 
-<<<<<<< HEAD
-def V_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-=======
 def V_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     """
     Calculate potential matrix of a given basis set.
@@ -128,7 +109,6 @@ def V_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     NDArray[np.float64]
         Potential matrix.
     """
->>>>>>> 4f58fba (BSE api.)
     n_CGTOs = basis_set.n_CGTOs
     S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
 
@@ -150,9 +130,6 @@ def V_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     return S_mat
 
 
-<<<<<<< HEAD
-def T_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-=======
 def T_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     """
     Calculate kinetic matrix of a given basis set.
@@ -167,7 +144,6 @@ def T_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     NDArray[np.float64]
         Kinetic matrix.
     """
->>>>>>> 4f58fba (BSE api.)
     n_CGTOs = basis_set.n_CGTOs
     S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
 
@@ -186,103 +162,6 @@ def T_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     return S_mat
 
 
-<<<<<<< HEAD
-def eri_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-    n_CGTOs = basis_set.n_CGTOs
-    dim = basis_set.matrix_indices[-1]
-    eri_tensor = np.zeros((dim, dim, dim, dim))
-
-    for i in range(n_CGTOs):
-        for j in range(n_CGTOs):
-            for k in range(n_CGTOs):
-                for l in range(n_CGTOs):
-
-                    index_i = basis_set.matrix_indices[i]
-                    index_i_stop = basis_set.matrix_indices[i + 1]
-                    index_j = basis_set.matrix_indices[j]
-                    index_j_stop = basis_set.matrix_indices[j + 1]
-                    index_k = basis_set.matrix_indices[k]
-                    index_k_stop = basis_set.matrix_indices[k + 1]
-                    index_l = basis_set.matrix_indices[l]
-                    index_l_stop = basis_set.matrix_indices[l + 1]
-
-                    eri_tensor[
-                        index_i:index_i_stop,
-                        index_j:index_j_stop,
-                        index_k:index_k_stop,
-                        index_l:index_l_stop,
-                    ] = Eri_GTO_tensor(
-                        basis_set.CGTOs[i],
-                        basis_set.CGTOs[j],
-                        basis_set.CGTOs[k],
-                        basis_set.CGTOs[l],
-                    )
-
-    return eri_tensor
-
-
-def S_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-    n_CGTOs = basis_set.n_CGTOs
-    S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
-
-    for i in range(n_CGTOs):
-        for j in range(n_CGTOs):
-
-            index_i = basis_set.matrix_indices[i]
-            index_i_stop = basis_set.matrix_indices[i + 1]
-            index_j = basis_set.matrix_indices[j]
-            index_j_stop = basis_set.matrix_indices[j + 1]
-
-            S_mat[index_i:index_i_stop, index_j:index_j_stop] = S_GTO_mat(
-                basis_set.CGTOs[i], basis_set.CGTOs[j]
-            )
-
-    return S_mat
-
-
-def V_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-    n_CGTOs = basis_set.n_CGTOs
-    S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
-
-    for i in range(n_CGTOs):
-        for j in range(n_CGTOs):
-
-            index_i = basis_set.matrix_indices[i]
-            index_i_stop = basis_set.matrix_indices[i + 1]
-            index_j = basis_set.matrix_indices[j]
-            index_j_stop = basis_set.matrix_indices[j + 1]
-
-            S_mat[index_i:index_i_stop, index_j:index_j_stop] = V_GTO_mat(
-                basis_set.CGTOs[i],
-                basis_set.CGTOs[j],
-                basis_set.r_atoms,
-                basis_set.q_atoms,
-            )
-
-    return S_mat
-
-
-def T_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-    n_CGTOs = basis_set.n_CGTOs
-    S_mat = np.zeros((basis_set.matrix_indices[-1], basis_set.matrix_indices[-1]))
-
-    for i in range(n_CGTOs):
-        for j in range(n_CGTOs):
-
-            index_i = basis_set.matrix_indices[i]
-            index_i_stop = basis_set.matrix_indices[i + 1]
-            index_j = basis_set.matrix_indices[j]
-            index_j_stop = basis_set.matrix_indices[j + 1]
-
-            S_mat[index_i:index_i_stop, index_j:index_j_stop] = T_GTO_mat(
-                basis_set.CGTOs[i], basis_set.CGTOs[j]
-            )
-
-    return S_mat
-
-
-def eri_basis_set(basis_set: BasisSet) -> NDArray[np.float64]:
-=======
 def eri_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     """
     Calculate electron repulsion integral (ERI) tensor of a given basis set.
@@ -297,7 +176,6 @@ def eri_basis_set(basis_set: BasisSetClass) -> NDArray[np.float64]:
     NDArray[np.float64]
         ERI tensor.
     """
->>>>>>> 4f58fba (BSE api.)
     n_CGTOs = basis_set.n_CGTOs
     dim = basis_set.matrix_indices[-1]
     eri_tensor = np.zeros((dim, dim, dim, dim))
